48d49c7ee80e16c01f8c2fe9a122fcd6b38faae8
Donjella <brendonang@gmail.com>
2025-06-29 16:54:47 +1000

    feat(index): connect to database before starting server

---
+++ src/database.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+async function databaseConnect(targetDatabaseURL = null){
+    console.log('Starting database connection');
+
+     const actualDatabaseURL = 
+     targetDatabaseURL || 
+     process.env.DATABASE_URL || 
+     process.env.MONGO_URI ||  
+     'mongodb://localhost:27017/feedback_board_db'
+
+     console.log(`Connecting to ${actualDatabaseURL}`);
+
+    try {
+        await mongoose.connect(actualDatabaseURL);
+    } catch (error) {
+        console.log ("Database conection error", error);
+        throw error;
+    }
+}
+
+async function databaseDisconnect() {
+    try {
+        await mongoose.disconnect();
+        console.log("Disconnected from MongoDB");
+        return true;
+    } catch (error) {
+        console.log("MongoDB disconnection error:", error);
+        throw error;
+    }
+}
+
+module.exports = {databaseConnect, databaseDisconnect};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 const { app } = require('./server.js');
 const dotenv = require ('dotenv');
+const { databaseConnect } = require ('./database.js');
 
 // Load environment variables
 dotenv.config();
 
-const PORT = process.env.PORT;
+// Set port
+const PORT = process.env.PORT || 5000;
 
-app.listen (PORT, () => {
+// Connect to the database before starting the server
+databaseConnect().then(() => {
+    app.listen (PORT, () => {
     console.log(`Server is running on port ${PORT}!` )
-});
+    })
+}).catch((error) => {
+    console.error('Failed to connect to MongoDB', error);
+    process.exit(1); // Stop the server if the database fails to connect
+})
+
