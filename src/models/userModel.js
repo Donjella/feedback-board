ddf58ac2d19e99c994d63e75f31bacf8e8004c31
Donjella <brendonang@gmail.com>
2025-07-02 01:50:22 +1000

    feat(user): add pre-save middleware to hash passwords using bcrypt

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema(
     {
@@ -39,6 +40,18 @@ const userSchema = new mongoose.Schema(
         timestamps: true,
     },
 );
+
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+    // Only hash if password is modified (or new)
+    if (!this.isModified('password')) {
+        return next();
+    }
+
+    // Hash password with salt rounds of 10
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+})
 
 const userModel = mongoose.model('User', userSchema);
 module.exports = userModel;
