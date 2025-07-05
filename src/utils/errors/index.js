0270caaa400bb7b7ed95d7e72257f895e65f7c7e
Donjella <brendonang@gmail.com>
2025-07-06 00:40:41 +1000

    feat(errors): add central export for custom error classes

---
+++ src/utils/errors/index.js
@@ -0,0 +1,14 @@
+const ConflictError = require('./conflictError');
+const ForbiddenError = require('./forbiddenError');
+const NotFoundError = require('./notFoundError');
+const UnauthorizedError = require('./unauthorizedError');
+const ValidationError = require('./validationError');
+
+module.exports = {
+    ConflictError,
+    ForbiddenError,
+    NotFoundError,
+    UnauthorizedError,
+    ValidationError,
+};
+
