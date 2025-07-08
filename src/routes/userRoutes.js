2b2ddf8d0003f4845696f791fae816147a8a13d8
Donjella <brendonang@gmail.com>
2025-07-09 01:13:05 +1000

    feat(routes): add login route for user authentication

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,9 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
-const { registerUser } = require('../controllers/userController');
+const { registerUser, loginUser } = require('../controllers/userController');
 
 // Public Routes (No Authentication Required)
 router.post('/register', registerUser);
+router.post('/login', loginUser);
 
 module.exports = router;
