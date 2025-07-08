c8ca8beafdd2d6544227e9a0fd44f6b03ccf2053
Donjella <brendonang@gmail.com>
2025-07-09 02:32:16 +1000

    feat(auth): add protect middleware for JWT authentication using custom error handling

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/userModel';
+import asyncHandler from 'express-async-handler';
+import { NotFoundError, UnauthorizedError } from '../utils/errors';
+
+// Middleware to protect routes (Users must be logged in)
+const protect = asyncHandler(async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    
+    if(!authHeader || !authHeader.startsWith('Bearer')) {
+        throw new UnauthorizedError('Not authorised, no token provided');
+    } else {
+
+    const [, token] = req.headers.authorization.split(' ');
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = await User.findById(decoded.id).select('-password');
+
+    if (!req.user){
+        throw new NotFoundError('Unauthorised. User not found');
+    }
+    }
+
+    next();
+})
+
